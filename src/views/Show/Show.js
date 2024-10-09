813bf6f91a24d51c02408d7a8a6c268f5a28de5a
swaroopatarkase <tarkaseswaroopa@gmail.com>
2024-10-09 14:40:44 +0530

    view notes page added

--- src/views/Show/Show.js
+++ src/views/Show/Show.js
@@ -1,10 +1,30 @@
-import React from 'react'
-import "./Show.css"
+import React from 'react';
+import './Show.css';
 
-function Show() {
+const Show = () => {
+  const Notes = JSON.parse(localStorage.getItem('Notes')) || [];
+
   return (
-    <div>Show</div>
-  )
-}
+    <div className='main-container'>
+      <div className='heading-txt add-heading'>
+        View Notes🗒️
+      </div>
+      <p className='tag-line'>View all your notes!</p>
+      <div className='notes-container'>
+        {Notes.length === 0 ? (
+          <p>No notes available. Start adding some!</p>
+        ) : (
+          Notes.map((note, index) => (
+            <div key={index} className='note-card'>
+              <h3 className='note-title'>{note.tittle}</h3>
+              <p className='note-category'>Category: {note.category}</p>
+              <p className='note-description'>{note.description}</p>
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default Show
+export default Show;
