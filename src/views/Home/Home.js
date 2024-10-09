08473707b375745e73111d4469c8b4386d2aef1a
swaroopatarkase <tarkaseswaroopa@gmail.com>
2024-10-09 13:50:09 +0530

    img and para added

---
+++ src/views/Home/Home.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Home.css';
+import left from './work.png';
+
+function Home() {
+  return (
+    <div>
+      <h1 className='text-center text-primary'>Quick Notes</h1>
+      <div className='text-center text-secondary'>
+        Simplify Your Day, Amplify Your Productivity.
+      </div>
+
+      <div className='main'>
+        <div>
+          <img src={left} alt='Work illustration' />
+        </div>
+        <div>
+          <p>
+            Welcome to Quick Notes, your go-to platform for capturing thoughts, ideas, and important information effortlessly! Whether you’re a busy professional, a student, or someone who loves to jot down insights, our user-friendly interface makes it easy to create, organize, and access your notes anytime, anywhere.
+          </p>
+          <p>
+            Enjoy seamless syncing across devices, customizable templates, and powerful search features that ensure you never lose track of what matters most. Start jotting down your ideas today and experience the convenience of Quick Notes!
+          </p>
+        </div>
+      </div>
+
+      <div className='button-container'>
+        <button className='btn-primary'>✍️ Add Note</button>
+        <button className='btn-primary'>🗒️ View Notes</button>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/views/Show/Show.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Show.css"
+
+function Show() {
+  return (
+    <div>Show</div>
+  )
+}
+
+export default Show
